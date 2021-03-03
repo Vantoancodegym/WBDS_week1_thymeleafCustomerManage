befcf841b5b6c511efcf38d4621672c1dfe0f710
vantoanftu93 <73921998+vantoanftu93@users.noreply.github.com>
2021-03-03 12:40:55 +0700

    done

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -3,10 +3,7 @@ package controller;
 import model.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import service.CustomerService;
 
@@ -32,10 +29,28 @@ public class UserController {
     }
     @PostMapping("create")
     public ModelAndView create(@ModelAttribute Customer cus){
-        int id=customerService.findAll().size();
+        int id=(int)(Math.random()*100000) ;
         cus.setId(id);
         customerService.save(cus);
-        return showAll();
+        return new ModelAndView("redirect:/customers");
+    }
+    @GetMapping("edit")
+    public ModelAndView showFormEdit(@RequestParam int id){
+        ModelAndView modelAndView=new ModelAndView("edit");
+        Customer cus=customerService.findById(id);
+        modelAndView.addObject("cus",cus);
+        return modelAndView;
+    }
+    @PostMapping("edit")
+    public ModelAndView edit(@RequestParam int id, @ModelAttribute Customer cus ){
+        cus.setId(id);
+        customerService.update(id,cus);
+        return new ModelAndView("redirect:/customers");
+    }
+    @GetMapping("delete")
+    public ModelAndView delete(@RequestParam int id){
+        customerService.remove(id);
+        return new ModelAndView("redirect:/customers");
     }
 
 }
